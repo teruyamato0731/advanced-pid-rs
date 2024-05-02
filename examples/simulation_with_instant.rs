ee8c38d496c9737a3eccffce4ae3a30e67c119fe
teruyamato0731 <teruyamato0731@gmail.com>
2024-05-02 04:10:15 +0000

    docs: Instantを使用したexampleを追加

---
+++ examples/simulation_with_instant.rs
@@ -0,0 +1,25 @@
+use advanced_pid::{vel_pid::VelPid, PidConfig, PidController};
+
+use std::time::Instant;
+
+fn main() {
+    let mut pid = VelPid::default();
+    let config = PidConfig::new(0.8, 0.3, 0.2).with_limits(-1.2, 1.2);
+    pid.reset_config(config);
+
+    let target = 1.0;
+    let mut actual = 0.0;
+
+    let mut pre = Instant::now();
+    loop {
+        let now = Instant::now();
+        let dt = now - pre;
+
+        if dt > std::time::Duration::from_secs(1) {
+            let output = pid.update(target, actual, dt.as_secs_f32());
+            actual += (output - actual) / 8.0;
+            println!("{:5.2}\t{:5.2}\t{:?}", actual, output, dt);
+            pre = now;
+        }
+    }
+}
